82eddc207628d918eb17fe302542e97d8ea58593
amandatsantos <tavaresamandasantos@hotmail.com>
2022-05-31 18:10:13 -0300

    friotec: secoes contato, carrosel, e rodapé

---
+++ script.js
@@ -0,0 +1,28 @@
+
+/* const images = [
+    { 'id':'1', 'url': './image/logo1.jpg'},
+    { 'id':'2', 'url': './image/logo1.jpg'}
+]
+const container = document.querySelector('.carrosel-servico')
+
+const loadImages = (images, container) => {
+     images.forEach(image => {  container.innerHTML = `<div> class=`
+         
+     });
+}
+loadImages(images, container);  */
+
+const imgs = document.getElementById('img')
+const img = document.querySelectorAll("#img img")
+
+let idx = 0;
+
+function carrosel() {
+    idx++;
+    if (idx > img.length - 1) {
+        idx = 0;
+    }
+    imgs.style.transform = `translateX(${-idx * 500}px)`
+}
+
+setInterval(carrosel, 1000);
